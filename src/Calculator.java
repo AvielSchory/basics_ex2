10f80fc1683b1607526e289b4cf2b62552dcf40d
yosef vaksin <yosefv08@gmail.com>
2025-12-05 11:44:20 +0200

    added pow

--- src/Calculator.java
+++ src/Calculator.java
@@ -10,20 +10,30 @@ public class Calculator {
     public static int multiply(int a, int b){
         return a * b;
     }
+    public static int pow(int a, int b){
+        int res=a;
+        for(int i=0; i<b; i++){
+            multiply(res, a);
+        }
+        return res;
+    }
   
     public static void main(String[] args) {
         final int EXIT = 0;
         final int ADD = 1;
         final int MULTIPLY = 2;
+        final int SUB =3; 
+        final int POW =5; 
         Scanner scanner = new Scanner(System.in);
         int choice;
 
         do {
             System.out.println("\n=== Calculator Menu ===");
-            System.out.println(ADD + ". Add");
-            System.out.println(SUB + ". sub");
             System.out.println(EXIT + ". Exit");
+            System.out.println(ADD + ". Add");
             System.out.println(MULTIPLY + ".Multiply");
+            System.out.println(SUB + ". sub");
+            System.out.println(POW + ".POW");
             System.out.print("Choose an option: ");
 
             choice = scanner.nextInt();
@@ -49,6 +59,12 @@ public class Calculator {
                     int a = scanner.nextInt();
                     int b = scanner.nextInt();
                     System.out.println("Result = " + multiply(a, b));
+                    break;
+                case POW:
+                    System.out.print("Enter two numbers: ");
+                    int a = scanner.nextInt();
+                    int b = scanner.nextInt();
+                    System.out.println("Result = " + pow(a, b));
                     break;
                 default:
                     System.out.println("Invalid choice, try again.");
